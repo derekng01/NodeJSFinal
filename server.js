fe01d874820fb7349c2656e63e4d037bdb6a4789
donkey nong <48866991+derekng01@users.noreply.github.com>
2019-04-17 12:27:22 -0700

    asd

--- server.js
+++ server.js
@@ -149,13 +149,6 @@ route.post('/get_image', async(request, response)=> {
     }
 });
 
-
-
-
-
-
-
-
 app.get('/', (request, response) => {
     response.render('main.hbs', {
         title: "Home Page",
@@ -163,11 +156,8 @@ app.get('/', (request, response) => {
     });
 });
 
-
-
-
 app.get('/2', (request, response) => {
-    response.render('second.hbs', {
+    response.render('gallery.hbs', {
         title: "Home Page",
         header: "Welcome to Home!"
     });
@@ -177,16 +167,6 @@ app.get('/404', (request, response) => {
     response.send({error: 'Page not found'})
 });
 
-
-// app.post('/input', (request, response)=> {
-//     var user_input = request.body;
-//     var user_input = JSON.stringify((user_input.user))
-//     console.log(user_input);
-//
-//     response.render('main.hbs', {
-//         output: user_input
-//     })
-// });
 
 app.listen(port, () => {
     console.log('Server is up on the port 8080')
