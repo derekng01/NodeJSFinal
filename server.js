3b8468c4db6ecea01e870442466bdb04542df694
donkey nong <48866991+derekng01@users.noreply.github.com>
2019-04-17 09:53:29 -0700

    asd

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const hbs = require('hbs');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 var app = express();
 const fs = require('fs');
 
 const port = process.env.PORT || 8080;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 
 hbs.registerPartials(__dirname);
 
@@ -26,7 +34,10 @@ app.get('/404', (request, response) => {
 
 
 app.post('/input', (request, response)=> {
-    user_input = request.body
+    var user_input = request.body;
+    var user_input = JSON.stringify((user_input.user))
+    console.log(user_input);
+
     response.render('main.hbs', {
         output: user_input
     })
